8e509c387fb26bb6e7024173a5e803cbb087d98a
Adam Williams <kainolophobia@gmail.com>
2014-08-01 14:36:30 -0700

    Updated to pass values to user profiles

--- server/hd_server.js
+++ server/hd_server.js
@@ -1,6 +1,11 @@
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Meteor.startup(function () {
+// code to run on server at startup
+});
+
+Accounts.onCreateUser(function(options, user) {
+  // We're enforcing at least an empty profile object to avoid needing to check
+  // for its existence later.
+  user.profile = options.profile ? options.profile : {};
+  return user;
+});
