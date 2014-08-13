ecc73ca99149f5ed802b3307c329fb0855451e02
Adam Williams <kainolophobia@gmail.com>
2014-08-13 04:05:43 +0000

    updated config with localhost stuff

--- server/config.js
+++ server/config.js
@@ -1,44 +1,16 @@
+
+
 ServiceConfiguration.configurations.remove({
     service : 'github'
 });
 ServiceConfiguration.configurations.insert({
     service : 'github',
-    clientId: 'b04325d8189b5abd4358',
-    secret  : '88f6cc75ef929df9406290194e700cb18ce2577b'
+//localhost
+    clientId: '6b42c70ff19b12a4e835',
+    secret  : '58bb8fc7b6d71ccade75f520b222efa67b26a49e'
+//live
+//    clientId: 'b04325d8189b5abd4358',
+//    secret  : '88f6cc75ef929df9406290194e700cb18ce2577b'
 });
 
-
-Accounts.onCreateUser(function (options, user) {
-  var accessToken = user.services.github.accessToken,
-      result,
-      profile;
-
-  result = Meteor.http.get("https://api.github.com/user", {
-    headers: {
-    	'User-Agent':'davidfurlong'
-    },
-    params: {
-      access_token: accessToken
-    }
-  });
-
-  if (result.error)
-    throw result.error;
-
-  profile = _.pick(result.data,
-    "login",
-    "name",
-    "avatar_url",
-    "url",
-    "company",
-    "blog",
-    "location",
-    "email",
-    "bio",
-    "html_url");
-
-  user.profile = profile;
-
-  return user;
-});
 
