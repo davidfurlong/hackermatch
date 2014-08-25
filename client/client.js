e99e977037a341badbf82851981eb470274305e7
Adam Williams <kainolophobia@gmail.com>
2014-08-24 20:14:25 -0700

    updated create_hackathon

--- client/client.js
+++ client/client.js
@@ -658,58 +658,7 @@ Template.admin.events({
         create_hackathon(title);
     }
 });
-    
-function create_hackathon(title) {
-    
-    console.log(title);
-
-    //check for valid title
-    if(!title) {
-        //Err out
-        return false;
-    }
-
-    var url_title = encodeURI(title.toLowerCase().replace(/ /g, ''));
-
-    var hackathon = {
-        title: title,
-        url_title: url_title
-/*
-        description: description,
-        userId: Meteor.userId(),
-        avatar_url: Meteor.user().profile.avatar_url,
-        skills: {
-            webdev: webdev,
-            backend: backend,
-            mobile: mobile,
-            design: design,
-            hardware: hardware
-        },
-        comments: {}
-*/
-    };
-
-    var exists = Hackathons.findOne({title: title});
-    if(exists) {
-        console.log("hackathon exists!");
-        //do something else
-        //can we select on name attribute again?
-    }
-
-    Hackathons.insert(hackathon, function(err, result) {
-        if(err) {
-            console.log("error creating hackathon");
-        } else {
-            console.log("hackathon created!");
-            
-            //reset input field
-        }
-
-//            Router.go('home');
-    });
-    return false;
-}
-
+   
 Template.idea_create_template.events({
     'submit #idea-create' : function(e, t) {
       e.preventDefault();
